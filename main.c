5562a173f38158bc68cb3805dc2833d0241dbbd9
alejoperezb <53632743+alejoperezb@users.noreply.github.com>
2019-08-02 22:47:56 -0500

    Holberton_Alejandro_perez_barona

--- main.c
+++ main.c
@@ -1,9 +1,32 @@
+#include <stdio.h>
 int main()
 {
   int i;
   for (i = 0; i < 10; i++)
   {
-    printf("Holberton\n");
+    printf("Hello Holberton");
+  }
+  return 0;
+}
+
+#include <stdio.h>
+int main()
+{
+  int i;
+  for (i = 0; i < 10; i++)
+  {
+    printf("I'm Alejandro Perez Barona");
+  }
+  return 0;
+}
+
+#include <stdio.h>
+int main()
+{
+  int i;
+  for (i = 0; i < 10; i++)
+  {
+    printf("The next software engineer");
   }
   return 0;
 }

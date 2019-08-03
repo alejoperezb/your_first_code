f33e23d5056b201c636e8a0ad8b5cd5230d4ff54
alejoperezb <53632743+alejoperezb@users.noreply.github.com>
2019-08-03 00:28:12 -0500

    Holberton-Alejandro

--- main.c
+++ main.c
@@ -1,35 +1,31 @@
-int main()
+#include<stadio.h>
+#include<strings.h>
+
+int main(int argc, char *argv[], char *envp[])
 {
   int i;
   for (i = 0; i < 10; i++)
   {
-    printf("Hello Holberton");
+    printf("Hello Holberto\n"), i, argv[i];
   }
-  return 0;
-}
-int main()
-{
-  int i;
-  for (i = 0; i < 10; i++)
+    while (envp) {
+      printf("Hello Holberto\n", *envp++)
+    }
   {
-    printf("Hello Holberton");
-  }
-  return 0;
-}
-int main()
-{
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("Hello Holberton");
-  }
-  return 0;
-}int main()
-{
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("Hello Holberton");
-  }
+   while (*envp) {
+     char *str = *envp++;
+     if (str[0] == "l") {
+       printf("Hello Holberto\n"), str;
+     }
+     
+    {
+   while (*envp) {
+     char *str = *envp++;
+     if (str[0] == "l" && str[1] == "o" 
+         && str[2] == "g" && str[3] == "g" 
+         && str[4] == "i" && str[5] == "n" 
+         && str[6] == "c"){
+       printf("Hello Holberto\n"), str;
+     } 
   return 0;
 }
